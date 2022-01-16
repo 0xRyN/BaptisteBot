58c06bd4a45ec97b42b6a1203dccbf1973bf3064
Rayan Al Azawi <ryanalaz11@gmail.com>
2022-01-16 23:44:50 +0100

    Separated consts

---
+++ consts.js
@@ -0,0 +1,45 @@
+const links = [
+    "https://cdn.discordapp.com/attachments/730773665066516490/919994972470652978/feur.mp4",
+    "https://cdn.discordapp.com/attachments/730773665066516490/919994147241353246/feur.mp4",
+    "https://cdn.discordapp.com/attachments/730773665066516490/919995774014722078/video0.mov",
+    "https://cdn.discordapp.com/attachments/753279793461592174/919996299883970590/yt1s.com_-_FEUR_masterclass_ici_cest_Pessi_.mp4",
+    "https://cdn.discordapp.com/attachments/730773665066516490/919997763524124742/VIDEO_FEUR.mp4",
+];
+const whitelist = [
+    "401703748109467648",
+    "363033061161435136",
+    "623246465287847956",
+    "122429924739907584",
+    "693079612627877939",
+];
+// So far whitelisted members are (in order) :
+/*
+    Uly#4465
+    Lyrelle#0512
+    Hipp0x#0668
+    PandorasRed#2564
+    Laure#0308
+*/
+
+const mails = {
+    "Abbes Samy": "samy.abbes@univ-paris-diderot.fr",
+    "Balthazar Bauer": "Bauer.Balthazar@irif.fr",
+    "Degorre Aldric": "Aldric.Degorre@irif.fr",
+    "Geoffroy Guillaume": "Guillaume.Geoffroy@irif.fr",
+    "Gonzalez Colin": "Colin.Gonzalez@irif.fr",
+    "Jurski Yan": "jurski@irif.fr",
+    "Laroussinie François": "François.Laroussinie@irif.fr",
+    "Letouzey Pierre": "Pierre.Letouzey@irif.fr",
+    "Mantaci Roberto": "Roberto.Mantaci@irif.fr",
+    "Micheli Anne": "Anne.MICHELI@irif.fr",
+    "Picantin Matthieu": "picantin@irif.fr",
+    "Poulalhon Dominique": "Dominique.Poulalhon@irif.fr",
+    "Rozière Paul": "Paul.Roziere@irif.fr",
+    "Sangnier Arnaud": "Arnaud.Sangnier@irif.fr",
+    "Sirangelo Cristina": "Cristina.Sirangelo@irif.fr",
+    "Zielonka Wieslaw": "Wieslaw.Zielonka@irif.fr",
+};
+
+exports.mails = mails;
+exports.whitelist = whitelist;
+exports.links = links;
